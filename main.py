ccc17bd47ddfbf308c2c9799112e1761f7ca78e6
Philippe GUYARD <abcdef0509@mail.ru>
2019-12-29 19:59:59 +0300

    Structural changes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if os.path.exists(constants.logs):
         shutil.copy(constants.logs, constants.logs_backup)
 logging.basicConfig(
     handlers=[logging.FileHandler(constants.logs, 'w', 'utf-8')],
-    format='%(levelname)s - %(asctime)s - %(message)s', level=logging.DEBUG)
+    format='%(levelname)s - %(asctime)s - %(message)s', level=logging.INFO)
 
 def my_handler(type, value, tb):
     logging.exception('Uncaught exception: {0}'.format(str(value)), exc_info=True)
@@ -24,10 +24,16 @@ sys.excepthook = my_handler
 def main():
     task_manager.load_tasks()
 
-    bot = telebot.TeleBot(constants.bot_token)
-
     get_sender_id = lambda msg: msg.from_user.id
     get_chat_id = lambda msg: msg.chat.id
+    get_basic_info = lambda msg: (get_sender_id(msg), get_chat_id(msg))
+
+    def listener(messages):
+        for m in messages:
+            logging.info('Got message {0} from user {1}'.format(m.text, get_sender_id(m)))
+
+    bot = telebot.TeleBot(constants.bot_token)
+    bot.set_update_listener(listener)
 
     def remove_markup(message, txt='Хорошо!'):
         markup = types.ReplyKeyboardRemove(selective=False)
@@ -37,57 +43,63 @@ def main():
     def welcome(message):
         bot.reply_to(message, 'Привет!')
 
-    @bot.message_handler(commands=['tasks'])
-    def show_task_params(message):
-        cid = get_chat_id(message)
-        uid = get_sender_id(message)
+    @bot.message_handler(commands=['show_tasks'])
+    def show_tasks(message):
+        uid, cid = get_basic_info(message)
+
+        logging.info('Showing all tasks to user {0}'.format(uid))
+
+        sender_id = get_sender_id(message)
+        if task_manager.has_tasks(sender_id):
+            text = task_manager.show_tasks(sender_id) #nice string with all tasks
+            
+            bot.send_message(cid, text)
+        else:
+            bot.send_message(cid, 'Ура, все задачи выполнены! Можно отдыхать :)')
+
+    @bot.message_handler(commands=['add_task'])
+    def add_task(message):
+        uid, cid = get_basic_info(message)
+        logging.info('Trying to add task for user {0}'.format(uid))
+
+        msg = bot.send_message(cid, 'Введи название задачи')
+        def process_task_name(task_msg):
+            task_text = task_msg.text
+            task_manager.add_task(get_sender_id(task_msg), task_text)
+            logging.info('Added task {0} to user {1}'.format(task_text, get_sender_id(task_msg)))
+            bot.send_message(get_chat_id(task_msg), 'Задача \'{0}\' добавлена успешно!'.format(task_text))
+
+        bot.register_next_step_handler(msg, process_task_name)
+
+    @bot.message_handler(commands=['remove_task'])
+    def remove_task(message):
+        uid, cid = get_basic_info(message)
+
+        if not task_manager.has_tasks(uid):
+            bot.send_message(cid, 'Все задачи выполнены. Удалять нечего')
+            return 
+
+        tasks = task_manager.get_tasks(uid)
 
         markup = types.InlineKeyboardMarkup(row_width=3)
-        itembtn1 = types.InlineKeyboardButton('Показать задачи на сегодня', callback_data='show')
-        itembtn2 = types.InlineKeyboardButton('Добавить задачу', callback_data='add')
-        itembtn3 = types.InlineKeyboardButton('Удалить задачу', callback_data='remove')
-        markup.add(itembtn1, itembtn2, itembtn3)
-        bot.send_message(cid, 'Что нужно сделать?', reply_markup=markup)
+        btns = [types.InlineKeyboardButton(task, callback_data=task) for task in tasks]
+        markup.add(*btns)
 
-        @bot.callback_query_handler(func=lambda call: call.data == 'add')
-        def add_task(call):
-            logging.info('Trying to add task for user {0}'.format(uid))
+        keyboard_message = bot.send_message(cid, 'Что удалить?', btns, reply_markup=markup)
 
-            task_manager.start_add_task(get_sender_id(message))   
-            bot.send_message(cid, 'Введи название задачи')
+        @bot.callback_query_handler(func=lambda call: True)
+        def remove(call):
+            logging.info('Removing task \'{0}\' from user {1}'.format(call.data, uid))
 
-        @bot.callback_query_handler(func=lambda call: call.data == 'show')
-        def show_tasks(call):
-            logging.info('Showing all tasks to user {0}'.format(uid))
+            task_manager.remove_task(uid, call.data)
+            bot.send_message(cid, 'Задача \'{0}\' успешно удалена!'.format(call.data))
 
-            sender_id = get_sender_id(message)
-            if task_manager.has_tasks(sender_id):
-                text = task_manager.show_tasks(sender_id) #nice string with all tasks
-                
-                bot.send_message(cid, text)
-            else:
-                bot.send_message(cid, 'Ура, все задачи выполнены! Можно отдыхать :)')
-
-        @bot.callback_query_handler(func=lambda call: call.data == 'remove')
-        def remove_task(call):
-            if not task_manager.has_tasks(uid):
-                bot.send_message(cid, 'Все задачи выполнены. Удалять нечего')
-                return 
-
-            tasks = task_manager.get_tasks(get_sender_id(message))
-
-            markup = types.InlineKeyboardMarkup(row_width=3)
-            btns = [types.InlineKeyboardButton(task, callback_data=task) for task in tasks]
-            markup.add(*btns)
-
-            bot.send_message(cid, 'Что удалить?', btns, reply_markup=markup)
-
-            @bot.callback_query_handler(func=lambda call: True)
-            def remove(call):
-                logging.info('Removing task \'{0}\' from user {1}'.format(call.data, uid))
-
-                task_manager.remove_task(uid, call.data)
-                bot.send_message(cid, 'Задача \'{0}\' успешно удалена!'.format(call.data))
+            new_tasks = task_manager.get_tasks(uid)
+            if tasks != new_tasks:
+                new_markup = types.InlineKeyboardMarkup(row_width=3)
+                btns = [types.InlineKeyboardButton(task, callback_data=task) for task in new_tasks]
+                new_markup.add(*btns)
+                bot.edit_message_reply_markup(chat_id=cid, message_id=keyboard_message.message_id, reply_markup = new_markup)
 
     #This should always be last decorator as this is like 'default' option in switch statement (handles any message)
     @bot.message_handler(func=lambda msg: True)
@@ -95,16 +107,11 @@ def main():
         sender_id = get_sender_id(message)
         text = message.text
         chat_id = message.chat.id
-        if task_manager.is_add_task(sender_id):
-            #notify user
-            task_manager.finish_add_tast(sender_id, text)
-            logging.info('Added task {0} to user {1}'.format(text, sender_id))
-            bot.send_message(chat_id, 'Задача \'{0}\' добавлена успешно!'.format(text))
-        else:
-            logging.info('Got unknown text {0} from user {1}'.format(text, sender_id))
-            bot.send_message(chat_id, 'Понял')    
+        logging.info('Got unknown text {0} from user {1}'.format(text, sender_id))
+        bot.send_message(chat_id, 'Понял')    
 
-
+    bot.enable_save_next_step_handlers(delay=2)
+    bot.load_next_step_handlers()
     bot.polling()
 
 if __name__ == "__main__":
--- task_manager.py
+++ task_manager.py
@@ -1,7 +1,6 @@
 import constants
 import json
 
-mode = dict()
 tasks = dict()
 
 def load_tasks():
@@ -19,23 +18,14 @@ def save_tasks():
     with open(constants.json_storage, 'w') as f:
         f.write(json.dumps(tasks))
 
-def is_add_task(user_id):
-    return user_id in mode and mode[user_id] == 'ADD_TASK'
-
 def has_tasks(user_id):
     return user_id in tasks and len(tasks[user_id]) > 0
 
-def start_add_task(user_id):
-    mode[user_id] = 'ADD_TASK'
-
-def finish_add_tast(user_id, task_name):
-    mode[user_id] = 'DEFAULT'
+def add_task(user_id, task_name):
     if user_id in tasks:
         tasks[user_id].append(task_name)
     else:
         tasks[user_id] = [task_name]
-    
-    save_tasks()
 
 def remove_task(user_id, task_name):
     if user_id in tasks:
@@ -50,7 +40,7 @@ def remove_task(user_id, task_name):
 
 #returns all user tasks inside list
 def get_tasks(user_id):
-    return tasks[user_id]
+    return list(tasks[user_id])
 
 def show_tasks(user_id):
     nice_task_string = 'Твои задачи:\n'
