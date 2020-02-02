b9cf1a8901b6ad299b6dcda8e8532bd62299d0a3
Philippe GUYARD <abcdef0509@mail.ru>
2020-02-02 20:52:41 +0300

    .gitignore is now working

--- constants.py
+++
@@ -1,8 +0,0 @@
-bot_token = '1039129866:AAEFygyDe6FaBUC1NcD00-JtAA7Uj8OhqmY'
-
-schedule_storage = 'schedule_data.txt'
-task_storage = 'data.txt'
-urgent_storage = 'urgent_data.txt'
-
-logs = 'Logs/app.log'
-logs_backup = 'Logs/backup.log'
